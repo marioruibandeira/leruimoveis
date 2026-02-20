9bea7f913cf96c0c9241d88ae7ff4faffb81c75d
Mário Bandeira <bandeira321@hotmail.com>
2026-02-20 19:46:00 +0000

    Update database configuration in settings.py

--- leruimoveis/settings.py
+++ leruimoveis/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'leruimov_db',
+        'NAME': '',
         'USER': 'root',
-        'PASSWORD': '@ContrabandFox01',
+        'PASSWORD': '@',
         'HOST': '127.0.0.1',             
         'PORT': '3306', 
         'OPTIONS':{
